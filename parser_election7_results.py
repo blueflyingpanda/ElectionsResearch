1681286101b7641378f56f14579361bb574678ee
bobenkom <81431562+bobenkom@users.noreply.github.com>
2021-10-31 03:38:03 +0300

    Correct parser

--- parser_election7_results.py
+++ parser_election7_results.py
@@ -80,6 +80,42 @@ def main():
     link_left = 'http://www.moscow-city.vybory.izbirkom.ru/region/izbirkom?action=show&root=1&tvd='
     link_right = '&vrn=27720002327736&prver=0&pronetvd=null&region=77&sub_region=77&type=424&report_mode=null'
     link_mids = (
+        '27720002327741',
+        '27720002327747',
+        '27720002327752',
+        '27720002327756',
+        '27720002327760',
+        '27720002327764',
+        '27720002327769',
+        '27720002327774',
+        '27720002327779',
+        '27720002327785',
+        '27720002327789',
+        '27720002327793',
+        '27720002327797',
+        '27720002327801',
+        '27720002327807',
+        '27720002327811',
+        '27720002327815',
+        '27720002327819',
+        '27720002327824',
+        '27720002327828',
+        '27720002327834',
+        '27720002327837',
+        '27720002327841',
+        '27720002327845',
+        '27720002327850',
+        '27720002327853',
+        '27720002327857',
+        '27720002327860',
+        '27720002327864',
+        '27720002327868',
+        '27720002327871',
+        '27720002327875',
+        '27720002327880',
+        '27720002327883',
+        '27720002327887',
+        '27720002327890',
         '27720002327894',
         '27720002327899',
         '27720002327905',
@@ -90,9 +126,9 @@ def main():
         '27720002327927',
         '27720002327932'
     )
-    single_mandate = 37
+    single_mandate = 1
     csv = 'name,single_mandate,votes,potential_voters,inside_voters,early_voters,outside_voters,attendance,early,outside,won\n'
-    file = open('data.csv', 'a')
+    file = open('data.csv', 'w')
     for link_mid in link_mids:
         file.write(csv)
         csv = parse_page(link_left + link_mid + link_right, single_mandate)
