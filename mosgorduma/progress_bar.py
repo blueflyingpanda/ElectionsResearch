65d72a969d0c0f404e6e4a236fe5af4e4bb0e7a7
Ilya <isagaidak@isgneuro.com>
2021-12-29 19:50:06 +0300

    progress bar

--- mosgorduma/progress_bar.py
+++ mosgorduma/progress_bar.py
@@ -1,4 +1,4 @@
-def show_progress_bar(single_mandate, mandates=45, tiles=100)
+def show_progress_bar(single_mandate, mandates=45, tiles=100):
     percent = int(single_mandate * tiles / mandates)
     space = tiles - percent
     print('  LOADING:', '[' + '|' * percent + ' ' * space + ']', str(percent) + '%','MANDATE:', str(single_mandate), end='\r')
