a51184eb8babadcdcba6d94fe62d063cc67ae14d
Ilya <isagaidak@isgneuro.com>
2022-01-28 21:39:05 +0300

    kicks

--- municipal/main.py
+++ municipal/main.py
@@ -3,6 +3,7 @@ from parser_election2017 import parse_election_results2017, parse_election_info2
 from parser_election2017 import browser as browser2017
 from parser_election2022 import parse_election2022
 from os import path
+import pandas as pd
 
 
 def main():
@@ -14,6 +15,12 @@ def main():
             parse_election_results2017()
         if not path.isfile('info2017.csv'):
             parse_election_info2017()
+        # JUST FOR KIckS
+        browser2017.get('http://www.moscow-city.vybory.izbirkom.ru/region/izbirkom?action=show&root=1&tvd=4774001137464&vrn=4774001137457&prver=0&pronetvd=null&region=77&sub_region=77&type=220&vibid=4774001137457&report_mode=null')
+        html_source = browser2017.page_source
+        df = pd.read_html(html_source, attrs = {'class': 'table-bordered table-sm dataTable no-footer'}, encoding='utf-8')
+        print(df)
+        df[0].to_csv('kicks.csv')
         browser2017.close()
     # elif sys.argv[1] == '2022':
         # if not path.isfile('data2022.csv'):
